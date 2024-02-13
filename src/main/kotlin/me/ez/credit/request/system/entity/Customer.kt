33fcad4219b41776dc958ecaba99e32a6b55850c
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2024-02-12 23:03:50 -0300

    feat: add sql migration

--- src/main/kotlin/me/ez/credit/request/system/entity/Customer.kt
+++ src/main/kotlin/me/ez/credit/request/system/entity/Customer.kt
@@ -4,7 +4,7 @@ import me.ez.credit.request.system.entity.Address
 import jakarta.persistence.*
 
 @Entity
-@Table(name = "Cliente")
+//@Table(name = "Cliente")
 data class Customer(
     @Column(nullable = false) var firstName: String = "",
     @Column(nullable = false) var lastName: String = "",
